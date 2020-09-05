5f9ff3b827f5ae881f79c1b2580c374cd67c6bf2
jiangWeixian <jiangweixian1994@gmail.com>
2020-09-05 10:16:09 +0800

    ✨ mutiple select

--- mac.shipin.sh
+++ mac.shipin.sh
@@ -1,11 +1,97 @@
 #!/bin/sh
 
-# git
-# https://git-scm.com/book/en/v2/Getting-Started-Installing-Git
+# Request confirm
 ask() {
   read -r -n 1 -p "❯ $1? y/n: " "$2"
   echo
 }
+# Renders a text based list of options that can be selected by the
+# user using up, down and enter keys and returns the chosen option.
+#
+# Arguments   : list of options, maximum of 256
+# Return value: selected index (0 for opt1, 1 for opt2 ...)
+selecteds=()
+function select_option() {
+
+  # little helpers for terminal print control and key input
+  ESC=$(printf "\033")
+  cursor_blink_on() { printf "$ESC[?25h"; }
+  cursor_blink_off() { printf "$ESC[?25l"; }
+  cursor_to() { printf "$ESC[$1;${2:-1}H"; }
+  print_unchecked_option() { printf "  ✗ $1 "; }
+  print_selected_unchecked_option() { printf "❯ ✗ $1"; }
+  print_selected_checked_option() { printf "❯ ✓ $1"; }
+  print_checked_option() { printf "  ✓ $1"; }
+  get_cursor_row() {
+    IFS=';' read -sdR -p $'\E[6n' ROW COL
+    echo ${ROW#*[}
+  }
+  key_input() {
+    read -s -n3 key 2>/dev/null >&2
+    if [[ $key = $ESC[A ]]; then echo up; fi
+    if [[ $key = $ESC[B ]]; then echo down; fi
+    if [[ $key = $ESC[D ]]; then echo left; fi
+    if [[ $key = $ESC[C ]]; then echo right; fi
+    if [[ $key = "" ]]; then echo enter; fi
+  }
+
+  # initially print empty new lines (scroll down if at bottom of screen)
+  for opt; do printf "\n"; done
+
+  # determine current screen position for overwriting the options
+  local lastrow=$(get_cursor_row)
+  local startrow=$(($lastrow - $#))
+
+  # ensure cursor and input echoing back on upon a ctrl+c during read -s
+  trap "cursor_blink_on; stty echo; printf '\n'; exit" 2
+  cursor_blink_off
+
+  local selected=0
+  while true; do
+    # print options by overwriting the last lines
+    local idx=0
+    for opt; do
+      cursor_to $(($startrow + $idx))
+      if [[ $idx -eq $selected && ! "${selecteds[@]}" =~ "$idx" ]]; then
+        print_selected_unchecked_option "$opt"
+      elif [[ $idx -eq $selected && "${selecteds[@]}" =~ "$idx" ]]; then
+        print_selected_checked_option "$opt"
+      elif [[ $idx -ne $selected && ! "${selecteds[@]}" =~ "$idx" ]]; then
+        print_unchecked_option "$opt"
+      else
+        print_checked_option "$opt"
+      fi
+      ((idx++))
+    done
+
+    # user key control
+    case $(key_input) in
+    enter) break ;;
+    right)
+      selecteds+=($selected)
+      ;;
+    left)
+      deleted=($selected)
+      selecteds=("${selecteds[@]/$deleted}") 
+      ;;
+    up)
+      ((selected--))
+      if [ $selected -lt 0 ]; then selected=$(($# - 1)); fi
+      ;;
+    down)
+      ((selected++))
+      if [ $selected -ge $# ]; then selected=0; fi
+      ;;
+    esac
+  done
+
+  # cursor position back to normal
+  cursor_to $lastrow
+  printf "\n"
+  cursor_blink_on
+}
+# git
+# https://git-scm.com/book/en/v2/Getting-Started-Installing-Git
 IS_INSTALL_GIT=y
 IS_CONFIG_GITHUB=y
 ask "install git" IS_INSTALL_GIT
@@ -65,6 +151,7 @@ fi
 
 # enhance keyboard
 IS_BIND_ARROWKEY=y
+ask "bind arrow key": IS_BIND_ARROWKEY
 if [ $IS_BIND_ARROWKEY = y ]; then
   echo "bindkey "\e\e[D" backward-word" >> ${ZDOTDIR:-$HOME}/.zshrc
   echo "bindkey "\e\e[C" forward-word" >> ${ZDOTDIR:-$HOME}/.zshrc
