fc0557c9f5964d1ed6b816f34c8c868004aed463
jiangWeixian <jiangweixian1994@gmail.com>
2020-12-27 18:33:20 +0800

    ✨ crontab

---
+++ scripts/cron/greet.sh
@@ -0,0 +1,3 @@
+# just greet
+
+osascript -e 'display notification "今天又是元气满满的一天" with title "早上好, '"$USER"'"'
---
+++ scripts/cron/hello.sh
@@ -0,0 +1,6 @@
+# just say hello
+
+# 1. touch /var/log/crontab.log
+# 2. chmod 777 /var/log/crontab.log
+# require sudo permission /var/log/crontab.log
+echo "`date` hello, jiangwei" >> /var/log/crontab.log
